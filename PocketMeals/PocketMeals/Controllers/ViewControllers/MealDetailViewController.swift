c79c7de2e64fde85dc8cf2b81fbe2106f63418e2
Jakob Haslam <jakehaslam38@gmail.com>
2021-10-13 15:57:32 -0600

    DeetailView displaying data

--- PocketMeals/PocketMeals/Controllers/ViewControllers/MealDetailViewController.swift
+++ PocketMeals/PocketMeals/Controllers/ViewControllers/MealDetailViewController.swift
@@ -11,11 +11,20 @@ class MealDetailViewController: UIViewController {
 
     // MARK: - Outlets
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var mealInstructionsTextView: UITextView!
     
     
     // MARK: - Properties
     var meals: Meals?
     var meal: [Meal?] = []
+    var mealToDisplay: Meal? {
+        didSet {
+            guard let mealToDisplay = mealToDisplay else { return }
+            mealNameLabel.text = mealToDisplay.name
+            mealInstructionsTextView.text = mealToDisplay.instructions
+            mealInstructionsTextView.isEditable = false
+        }
+    }
     
     
     override func viewDidLoad() {
@@ -28,11 +37,14 @@ class MealDetailViewController: UIViewController {
         guard let meal = meals?.mealID else { return }
         
         MealController.fetchMeal(for: meal) { result in
-            switch result {
-            case .success(let meal):
-                self.meal = meal
-            case .failure(let error):
-                print(error, error.localizedDescription)
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let meal):
+                    self.meal = meal
+                    self.mealToDisplay = meal[0]
+                case .failure(let error):
+                    print(error, error.localizedDescription)
+                }
             }
         }
     }
